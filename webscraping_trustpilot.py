60b008ec2c84f60399ac39fd971a1c491e13ae15
hetvi <hetviptridhyatech@gmail.com>
2021-08-25 17:06:12 +0530

    final commit

--- webscraping_trustpilot.py
+++ webscraping_trustpilot.py
@@ -1,19 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
-
-
 import mysql.connector
-
 def createConn():
     return mysql.connector.connect(host = "localhost", user = "root",passwd = "123456789", database = "scraping")
-
 url = "https://www.trustpilot.com/categories/electronics_technology"
 res = requests.get(url)
 soup = BeautifulSoup(res.content,'html5lib')
 
-data = soup.find_all('a', attrs = {'class':'link_internal__YpiJI link_wrapper__LEdx5'})  #a with class specified
-#print(data)
-
+data = soup.find_all('a', attrs = {'class':'link_internal__YpiJI link_wrapper__LEdx5'}) 
 for e,i in enumerate(data):
     try:
         title=i.find('div', {"class" : 'styles_businessTitle__1IANo'}).text
@@ -23,7 +17,6 @@ for e,i in enumerate(data):
         review,star=reviewlist[0],reviewlist[4]
         print(title," : ",review, " : ",star)
         tup1 = (title,desc,review,star)
-
         conn = createConn()
         cursor = conn.cursor()
         query = "insert into trustpilot (title,Description,reviews,star_rating) values(%s,%s,%s,%s)"
